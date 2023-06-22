63a1277f0c5b7080acb651aaf53a53e0fee15a07
root <root@ip-172-31-11-141.eu-west-2.compute.internal>
2023-06-22 17:52:34 +0000

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,73 +1,75 @@
 export const environment = {
-  production: false,
-  idam_client_id: '',
-  uri: {
-    api: {
-      isApiGateWayEnabled: false,
-      security: 'https://localhost:44352',
-      postgres: 'https://localhost:44330',
-      wrapper: {
-        apiGatewayEnabled: {
-          user: 'https://localhost:44309/user-profiles',
-          organisation: 'https://localhost:44309/organisation-profiles',
-          contact: 'https://localhost:44309/contacts',
-          configuration: 'https://localhost:44309/configurations',
-          dataMigration: 'https://localhost:44309/datamigration',
-        },
-        apiGatewayDisabled: {
-          user: 'https://localhost:44309/users',
-          organisation: 'https://localhost:44309/organisations',
-          contact: 'https://localhost:44309/contacts',
-          configuration: 'https://localhost:44309/configurations',
-          dataMigration: 'https://localhost:44309/datamigration',
+    production: true,
+    idam_client_id:'CWwA7Q84GPlD1nF4ufxo6IDO6PIItiyS',
+    uri: {
+      api: {
+        isApiGateWayEnabled: false,
+        security: 'http://ppg-sand-security-alb-216831028.eu-west-2.elb.amazonaws.com',
+        postgres: 'http://ppg-sand-core-alb-467822619.eu-west-2.elb.amazonaws.com',
+        cii: 'https://conclave-cii-testing-talkative-oryx-hh.london.cloudapps.digital',
+        wrapper: {
+          apiGatewayEnabled: {
+            user: 'http://ppg-sand-wrapper-alb-1485365645.eu-west-2.elb.amazonaws.com/user-profiles',
+            organisation: 'http://ppg-sand-wrapper-alb-1485365645.eu-west-2.elb.amazonaws.com/organisation-profiles',
+            contact: 'http://ppg-sand-wrapper-alb-1485365645.eu-west-2.elb.amazonaws.com/contacts',
+            configuration: 'http://ppg-sand-wrapper-alb-1485365645.eu-west-2.elb.amazonaws.com/configurations',
+            dataMigration: 'http://ppg-sand-wrapper-alb-1485365645.eu-west-2.elb.amazonaws.com/datamigration',
+          },
+          apiGatewayDisabled: {
+            user: 'http://ppg-sand-wrapper-alb-1485365645.eu-west-2.elb.amazonaws.com/users',
+            organisation: 'http://ppg-sand-wrapper-alb-1485365645.eu-west-2.elb.amazonaws.com/organisations',
+            contact: 'http://ppg-sand-wrapper-alb-1485365645.eu-west-2.elb.amazonaws.com/contacts',
+            configuration: 'http://ppg-sand-wrapper-alb-1485365645.eu-west-2.elb.amazonaws.com/configurations',
+            dataMigration: 'http://ppg-sand-wrapper-alb-1485365645.eu-west-2.elb.amazonaws.com/datamigration',
+          }
         }
-      }
+      },
+      web: {
+        dashboard: 'http://ppg-sand-ccs-sso-alb-770694470.eu-west-2.elb.amazonaws.com'
+      },
+      ccsContactUrl: "https://www.crowncommercial.gov.uk/contact"
     },
-    web: {
-      dashboard: 'http://localhost:4200'
+    googleTagMangerId: 'GTM-TZCX5VP',
+    cookieExpirationTimeInMinutes: 525600,
+    bulkUploadPollingFrequencyInSeconds: 5,
+    bulkUploadMaxFileSizeInBytes:1048576,
+    bulkUploadTemplateFileUrl: 'https://sbx.api.crowncommercial.gov.uk/templates/DataMigrationTemplate.csv',
+    usedPasswordThreshold: 5, //This value should be changed when Auth0 password history policy changed,
+    listPageSize: 10,
+    mailDecryptKey:'conclavesimpleemailencrypt',
+    rollbar: {
+      key: '39bb38f087ee4a6781b9d76a73200dd8',
+      enable : false,
+      security_log: false,
+      environment: 'sandbox'
     },
-    ccsContactUrl: "https://www.crowncommercial.gov.uk/contact"
-  },
-  googleTagMangerId: 'GTM-1',
-  cookieExpirationTimeInMinutes: 10,// 525600 => 365 Days
-  bulkUploadPollingFrequencyInSeconds: 60,
-  bulkUploadTemplateFileUrl: '',
-  bulkUploadMaxFileSizeInBytes: 1048576,//default value
-  usedPasswordThreshold: 5, //This value should be changed when Auth0 password history policy changed,
-  listPageSize: 10,
-  mailDecryptKey: '',
-  rollbar: {
-    key: '',
-    enable: false,
-    security_log: false,
-    environment: 'dev-ccs-sso'
-  },
-  cookies_policy: {
-    essentialcookies: {
-      notify_admin_session: 20,
-      cookie_policy: 1,
-      ccs_sso_visitedsites: 30,
-      opbs: 30,
-      ccs_sso: 30,
-      conclave: 30,
-      XSRF_TOKEN: 30,
-      XSRF_TOKEN_SVR: 30,
-      AspNetCore_Antiforgery_GWNWkbbyKbw: 30
+    cookies_policy: {
+      essentialcookies: {
+          notify_admin_session: 20,
+          cookie_policy: 1,
+          ccs_sso_visitedsites: 60,
+          opbs: 60,
+          ccs_sso: 60,
+          conclave: 60,
+          XSRF_TOKEN: 30,
+          XSRF_TOKEN_SVR: 30,
+          AspNetCore_Antiforgery_GWNWkbbyKbw: 30
+      },
+      Auth0cookies: {
+          auth0_compat: 3,
+          did_compat: 6,
+          did: 6,
+          auth0: 3,
+          __cf_bm: 30
+      },
     },
-    Auth0cookies: {
-      auth0_compat: 3,
-      did_compat: 6,
-      did: 6,
-      auth0: 3,
-      __cf_bm: 30
+    appSetting: {
+     hideIDP:false,
+     hideDelegation:false,
+     hideBulkupload:false,
+     hideAutoValidation:false,
+     hideSimplifyRole:false,
+     blockedScheme: ['GB-EDU',"GB-PPG"]
     },
-  },
-  appSetting: {
-    hideIDP: false,
-    hideDelegation: false,
-    hideBulkupload: false,
-    hideAutoValidation: false,
-    hideSimplifyRole: false,
-    blockedScheme: ['GB-EDU',"GB-PPG"]
-  },
-};
+  };
+  
