758f3fa587ba579cd9bf8c9f972ed93c9e5ad377
ponselvamsakthivel-bc <87376328+ponselvamsakthivel-bc@users.noreply.github.com>
2023-07-14 16:18:48 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,23 +4,23 @@ export const environment = {
     uri: {
       api: {
         isApiGateWayEnabled: false,
-        security: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5004',
-        postgres: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5001',
+        security: 'https://sbx.api.crowncommercial.gov.uk/security',
+        postgres: 'https://sbx.api.crowncommercial.gov.uk/dashboardwrapper',
         cii: 'https://conclave-cii-testing-talkative-oryx-hh.london.cloudapps.digital',
         wrapper: {
           apiGatewayEnabled: {
-            user: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5000/user-profiles',
-            organisation: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5000/organisation-profiles',
-            contact: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5000/contacts',
-            configuration: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5000/configurations',
-            dataMigration: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5000/datamigration',
+            user: 'https://sbx.api.crowncommercial.gov.uk/wrapper/user-profiles',
+            organisation: 'https://sbx.api.crowncommercial.gov.uk/wrapper/organisation-profiles',
+            contact: 'https://sbx.api.crowncommercial.gov.uk/wrapper/contacts',
+            configuration: 'https://sbx.api.crowncommercial.gov.uk/wrapper/configurations',
+            dataMigration: 'https://sbx.api.crowncommercial.gov.uk/wrapper/datamigration',
           },
           apiGatewayDisabled: {
-            user: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5000/users',
-            organisation: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5000/organisations',
-            contact: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5000/contacts',
-            configuration: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5000/configurations',
-            dataMigration: 'http://sand-alb-backend-91858601.eu-west-2.elb.amazonaws.com:5000/datamigration',
+            user: 'https://sbx.api.crowncommercial.gov.uk/wrapper/users',
+            organisation: 'https://sbx.api.crowncommercial.gov.uk/wrapper/organisations',
+            contact: 'https://sbx.api.crowncommercial.gov.uk/wrapper/contacts',
+            configuration: 'https://sbx.api.crowncommercial.gov.uk/wrapper/configurations',
+            dataMigration: 'https://sbx.api.crowncommercial.gov.uk/wrapper/datamigration',
           }
         }
       },
